d9e0ea4c14654c50c7b06e71a2d9d8d65282109d
Shinpanda <duldulgo1@gmail.com>
2023-01-07 02:17:09 +0900

    chart

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -16,6 +16,8 @@ import Price from "./Price";
 
 const Container = styled.div`
   padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
 `;
 
 const Header = styled.header`
@@ -206,10 +208,10 @@ function Coin() {
 
           <Switch>
             <Route path={`/${coinId}/price`}>
-              <Price />
+              <Price coinId={coinId} />
             </Route>
             <Route path={`/${coinId}/chart`}>
-              <Chart />
+              <Chart coinId={coinId} />
             </Route>
           </Switch>
         </>
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -6,6 +6,8 @@ import { fetchCoins } from "../api";
 
 const Container = styled.div`
   padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
 `;
 
 const Header = styled.header`
